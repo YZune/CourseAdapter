f143b57f9aabbd46a3b43c43ad0ac0d6f1d58488
YZune <windowsyangzeng@hotmail.com>
2020-01-09 23:38:43 +0800

    新正方教务

--- src/NewZFTest.kt
+++ src/NewZFTest.kt
@@ -1,3 +1,23 @@
+import parser.NewZFParser
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    val path = "/Users/yzune/YZune_Git/database/python/schools/武汉轻工大学"
+    val file = File(path)
+    val fs = file.listFiles()
+    fs?.forEach {
+        if (it.isDirectory) return
+        val content = it.readText()
+        try {
+            NewZFParser(content).saveCourse()
+            println("↑" + it.name)
+            println()
+        } catch (e: Exception) {
+            if (content.contains("星期一") && content.contains("星期二") && content.contains("节次")) {
+                e.printStackTrace()
+                println("↑" + it.name)
+                println()
+            }
+        }
+    }
 }

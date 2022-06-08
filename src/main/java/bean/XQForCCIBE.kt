e237ca0296673bf9a84ae484c4fe5b47ea72e259
eucaly <1570193975@qq.com>
2022-06-08 18:08:21 +0800

    适配重庆对外经贸学院 json获取来自 'https://my.vpn.ccibe.edu.cn/api/manager/synchronize/synchronizestudent/curriculumNew' 修改Generator类, 对课程时间，课程max和周max进行修改

---
+++ src/main/java/bean/XQForCCIBE.kt
@@ -0,0 +1,24 @@
+package main.java.bean
+
+/**
+
+ * 重庆对外 学期json
+
+ * @name XQ
+
+ * @author eucaly
+
+ * @date 2022-06-08 15:13
+
+ */
+data class XQForCCIBE (
+    val id:String,
+    val startYear:String,
+    val endYear:String,
+    val startTime:String,
+    val endTime:String,
+    val trem:String,
+    val title:String,
+    val allWeek:String,
+    val nowWeek:String
+)

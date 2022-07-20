d1cffd77d065437a2961d728879797a520e40750
stevenlele <stevenlele@outlook.com>
2022-07-21 07:00:00 +0800

    THUParser: cleanup, drop support for PKU courses

--- src/main/java/test/THUTest.kt
+++ src/main/java/test/THUTest.kt
@@ -2,9 +2,10 @@ package main.java.test
 
 import parser.THUParser
 import java.io.File
+import java.nio.charset.Charset
 
 fun main() {
-    val source = File("../本科生选课系统.html").readText()
+    val source = File("../本科生选课系统.html").readText(Charset.forName("gb2312"))
     THUParser(source).apply {
         generateCourseList().forEach { println(it) }
         saveCourse()

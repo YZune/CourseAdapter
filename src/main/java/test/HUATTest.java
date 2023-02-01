23addeeeba8903751e9db92b00bc7f89a8d787a3
NekoRectifier <hyx123250@gmail.com>
2023-02-01 16:48:09 +0800

    Weeks array boundary bug fix

--- src/main/java/test/HUATTest.java
+++ src/main/java/test/HUATTest.java
@@ -5,8 +5,8 @@ import main.java.parser.HUATParser;
 public class HUATTest {
 
     public static void main (String[] args) {
-        HUATParser parser = new HUATParser("<table id=\"ctl00_ContentPlaceHolder1_CourseTable\" cellspacing=\"0\" cellpadding=\"0\" rules=\"all\" border=\"1\"\n" +
-                "    style=\"border-color:Black;border-width:1px;border-style:Solid;border-collapse:collapse;\"> 省略了...这次是311行的HTML </table>");
+        HUATParser parser = new HUATParser(" </table>");
         System.out.println(parser.generateCourseList());
+
     }
 }

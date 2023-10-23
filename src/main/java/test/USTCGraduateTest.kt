aff1299e78e3c2319c1dfb45a19d58d103a6a1eb
foresee-io <ysfcore@outlook.com>
2023-10-23 21:28:54 +0800

    fix 单双周问题 #中国科学技术大学研究生
    - 错误预检 行内单元格不足、时间格式不正确
    - 抽象 `Common.TimeHM` 类以简化主干

--- src/main/java/test/USTCGraduateTest.kt
+++ src/main/java/test/USTCGraduateTest.kt
@@ -9,8 +9,9 @@ fun main() {
     // 提交时一定不要上传 html 文件，涉及隐私问题
     /*
     请使用FireFox的SingleFile扩展，而不是Ctrl+S，去保存完整的HTML.
+    将filePath修改为HTML文件的路径.
     */
-    val file = File("./yjs1.html")
-    val parser = USTCGraduateParser(file.readText())
+    val filePath = "${System.getProperty("user.home")}/Downloads/yjs1.html"
+    val parser = USTCGraduateParser(File(filePath).readText())
     parser.saveCourse()
 }

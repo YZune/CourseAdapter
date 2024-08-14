e139fa494627c2f3f8c59b3589978a1d1db14b7d
yzune <windowsyangzeng@hotmail.com>
2024-08-14 23:24:26 +0800

    clean project

--- course_adapter/src/jvmTest/kotlin/Test.kt
+++ course_adapter/src/jvmTest/kotlin/Test.kt
@@ -1,3 +1,7 @@
-fun main() {
-    println("hello world!")
+import parser.THUParser
+import java.io.File
+
+// 请不要提交对此文件的修改
+suspend fun main() {
+    THUParser(File("文件路径").readText()).saveCourse()
 }

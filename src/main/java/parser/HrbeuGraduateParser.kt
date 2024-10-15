e53296260af93104bc74efe5fbc96e97e898e519
liheji <930617673@qq.com>
2024-10-16 00:35:47 +0800

    重构并fix：武汉理工大学研究生教务解析错误

--- src/main/java/parser/HrbeuGraduateParser.kt
+++ src/main/java/parser/HrbeuGraduateParser.kt
@@ -9,106 +9,103 @@ import parser.Parser
  * 仅根据哈尔滨工程大学研究生院课表解析而来，不代表全部可用
  */
 class HrbeuGraduateParser(source: String) : Parser(source) {
+    private val moreTeacher = "一班多师"
     private val brTrimRegex = Regex("(^<br>)|(<br>$)")
-    private val numNodeRegex = Regex("\\d+")
-    private val numRangeRegex = Regex("(\\d+)(-(\\d+))?")
+    private val numNodeRegex = Regex("(\\d+)(-(\\d+))?")
+    private val numWeekRegex = Regex("(\\d+)(-(\\d+))?")
     private val teachRegex = Regex("[(（](.*)[)）]")
-    private val gradeRegex = Regex("([\\d]+班)")
+    private val gradeRegex = Regex("(\\d+班)")
 
     override fun generateCourseList(): List<Course> {
         val doc = org.jsoup.Jsoup.parse(source)
         val table1 = doc.getElementById("StuCul_TimetableQry_TimeTable").getElementsByClass("WtbodyZlistS")[0]
         val trs = table1.getElementsByTag("tr")
-        val courseList = ArrayList<Course>()
-        for (tr in trs) {
+        val courseList = mutableListOf<Course>()
+        trs.forEach { tr ->
             val tds = tr.getElementsByTag("td")
-            var countDay = 0
-            for (i in 1 until tds.size) {
-                val courseSource = tds[i].html().replace(brTrimRegex, "").trim()
-                if (courseSource.isEmpty()) {
-                    countDay++
-                    continue
+            var dayIndex = 0
+            tds.drop(1).forEach { td ->
+                val courseSource = td.html().replace(brTrimRegex, "").trim()
+                if (courseSource.isNotEmpty()) {
+                    courseSource.split(Regex("<br>\\s*<br>")).forEach { course ->
+                        val split = course.split("<br>")
+                        if (split.size >= 5) {
+                            courseList.addAll(parseCourseInfo(split, dayIndex))
+                        }
+                    }
                 }
-
-                for (course in courseSource.split(Regex("<br>\\s*<br>"))) {
-                    val split = course.split("<br>")
-                    if (split.isEmpty() || split.size < 5) continue
-                    courseList.addAll(parseCourseInfo(split, countDay))
-                }
-
-                countDay++
+                dayIndex++
             }
         }
         return courseList
     }
 
-    private fun parseCourseInfo(split: List<String>, countDay: Int): List<Course> {
-        val resCourseList = ArrayList<Course>()
-
+    private fun parseCourseInfo(split: List<String>, dayIndex: Int): MutableList<Course> {
         //去除类型词
-        val splitEnd = ArrayList<String>()
-        for (it in split) {
-            splitEnd.add(it.substringAfter(':'))
-        }
+        val splitEnd = split.map { it.substringAfter(':').trim() }.toMutableList()
         //获取信息
         //增加班级文字
-        val grade: MatchResult? = gradeRegex.find(splitEnd[1])
+        val grade = gradeRegex.find(splitEnd[1])?.groupValues?.get(1)
         if (grade != null) {
-            splitEnd[0] += "(" + grade.groupValues[1] + ")";
+            splitEnd[0] += "($grade)"
         }
 
-        val numMacher = numNodeRegex.findAll(splitEnd[2])
-        val startNode: Int = numMacher.first().groupValues[0].toInt()
-        val endNode: Int = numMacher.last().groupValues[0].toInt()
-        if ("一班多师" in splitEnd[1]) {
-            for (item in splitEnd[3].split(";")) {
-                numRangeRegex.findAll(item).forEach {
-                    val values = it.groupValues
-                    val startWeek: String = values[1].trim()
-                    val endWeek: String = if (values[3].trim() == "") values[1] else values[3].trim()
+        val resList = mutableListOf<Course>()
+        // 获取所有课程区间
+        val allNodeList = numNodeRegex.findAll(splitEnd[2]).map { matchResult ->
+            val startNode = matchResult.groupValues[1].toInt()
+            val endNode = matchResult.groupValues[3].takeIf { it.isNotEmpty() }?.toInt() ?: startNode
+            intArrayOf(startNode, endNode)
+        }.toMutableList()
+        // 合并区间 [1,1]和[2,2] -> [1,2]
+        mergeCourse(allNodeList).forEach { (startNode, endNode) ->
+            if (moreTeacher in splitEnd[1]) {
+                for (item in splitEnd[3].split(";")) {
+                    val teacher = teachRegex.find(item)?.groupValues?.get(1) ?: ""
+                    resList.addAll(parseItem(item, splitEnd, dayIndex, teacher, startNode, endNode, split))
+                }
+            } else {
+                val teacher = splitEnd[1].substringBeforeLast(" ").trim()
+                resList.addAll(parseItem(splitEnd[3], splitEnd, dayIndex, teacher, startNode, endNode, split))
+            }
+        }
 
-                    val teacher = teachRegex.find(item)!!.groupValues[1]
-                    resCourseList.add(
-                        Course(
-                            splitEnd[0].trim(),
-                            countDay,
-                            splitEnd[4].trim(),
-                            teacher.trim(),
-                            startNode,
-                            endNode,
-                            startWeek.toInt(),
-                            endWeek.toInt(),
-                            0,
-                            0f,
-                            split.subList(5, split.size).joinToString().trim()
-                        )
-                    )
+        return resList
+    }
+
+    private fun parseItem(
+        item: String, splitEnd: MutableList<String>, dayIndex: Int,
+        teacher: String, startNode: Int, endNode: Int, split: List<String>
+    ): List<Course> {
+        // 所有周
+        return numWeekRegex.findAll(item).map { matchResult ->
+            val startWeek = matchResult.groupValues[1].toInt()
+            val endWeek = matchResult.groupValues[3].takeIf { it.isNotEmpty() }?.toInt() ?: startWeek
+
+            Course(
+                splitEnd[0], dayIndex, splitEnd[4], teacher, startNode, endNode,
+                startWeek, endWeek, 0, 0f, split.subList(5, split.size).joinToString()
+            )
+        }.toList()
+    }
+
+    private fun mergeCourse(intervals: MutableList<IntArray>): MutableList<IntArray> {
+        // 排序
+        intervals.sortBy { it.first() }
+        val ans = mutableListOf<IntArray>()
+
+        for (i in intervals.indices) {
+            if (ans.none()) ans += intervals[i]
+            else {
+                val lastTo = ans.last()[1]
+                val (from, to) = intervals[i]
+                if (lastTo + 1 >= from)
+                    ans.last()[1] = lastTo.coerceAtLeast(to)
+                else {
+                    ans += intervals[i]
                 }
             }
-        } else {
-            val teacher = splitEnd[1].substringBeforeLast(" ").trim()
-            numRangeRegex.findAll(splitEnd[3]).forEach {
-                val values = it.groupValues
-                val startWeek: String = values[1].trim()
-                val endWeek: String = if (values[3].trim() == "") values[1] else values[3].trim()
-                resCourseList.add(
-                    Course(
-                        splitEnd[0].trim(),
-                        countDay,
-                        splitEnd[4].trim(),
-                        teacher.trim(),
-                        startNode,
-                        endNode,
-                        startWeek.toInt(),
-                        endWeek.toInt(),
-                        0,
-                        0f,
-                        split.subList(5, split.size).joinToString().trim()
-                    )
-                )
-            }
         }
-
-        return resCourseList
+        return ans
     }
 }

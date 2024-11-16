20539f41b6d1711a56c1a6063412417bd483b367
kahvia-d <xuedongtan@163.com>
2024-11-16 17:43:58 +0800

    完成了对课表的适配

--- src/main/java/test/CTGUTEST.kt
+++ src/main/java/test/CTGUTEST.kt
@@ -2,6 +2,8 @@ package main.java.test
 
 import main.java.parser.CTGUParser
 
+// 三峡大学研究生课表适配
+
 fun main() {
     // 一个网页保存后有xxx.html文件和其资源文件夹xxx_files，课表在网页xxx.html中是iframe内嵌形式，因此课表文件实际是在资源文件夹中的home.html
     // 此处传入保存下来的html文件，parser内部会自动寻找资源文件夹下的 home.html 并生成课表

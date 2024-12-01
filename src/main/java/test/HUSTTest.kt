a7903981b7f3cd1b1368fb14db85df3f40e214fa
GoForceX <goforcex@outlook.com>
2024-12-01 17:50:39 +0800

    fix: 迁移到华中科技大学新教务系统

--- src/main/java/test/HUSTTest.kt
+++ src/main/java/test/HUSTTest.kt
@@ -4,7 +4,7 @@ import main.java.parser.HUSTParser
 import java.io.File
 
 fun main() {
-    val file = File("E:\\Temple\\hust.course.html")
+    val file = File("E:\\hust.course.html")
     val parser = HUSTParser(file.readText())
     parser.saveCourse()
 
